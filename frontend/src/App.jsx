ba284008356c1d098643dc10cff1a249eba9ca61
TanKittipat <kittipat12tan@gmail.com>
2024-10-07 14:52:50 +0700

    Store distance & click select store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import {
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import homeIcon from "./assets/home.png";
+import Swal from "sweetalert2";
 
 function App() {
   const center = [13.838510043535697, 100.02535680572677];
@@ -25,6 +27,88 @@ function App() {
         lng: position.coords.longitude,
       });
     });
+  };
+
+  const [deliveryZone, setDeliveryZone] = useState({
+    lat: "",
+    lng: "",
+    radius: 0,
+  });
+
+  // function to calculate distance between 2 points using Haversine Formular
+  const calculateDistance = (lat1, lat2, lng1, lng2) => {
+    const R = 6371e3; // Earth radius in meters
+    const phi_1 = (lat1 * Math.PI) / 180;
+    const phi_2 = (lat2 * Math.PI) / 180;
+
+    const delta_phi = ((lat2 - lat1) * Math.PI) / 180;
+    const delta_lambda = ((lng2 - lng1) * Math.PI) / 180;
+
+    const a =
+      Math.sin(delta_phi / 2) * Math.sin(delta_phi / 2) +
+      Math.cos(phi_1) *
+        Math.cos(phi_2) *
+        Math.sin(delta_lambda / 2) *
+        Math.sin(delta_lambda / 2);
+
+    const c = 2 * Math.atan(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c; // Distance in meters
+  };
+
+  const handleCheckZone = () => {
+    if (myLocation.lat === "" || myLocation.lng === "") {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Error!",
+        text: "Please enter your valid location.",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return;
+    }
+    if (deliveryZone.lat === "" || deliveryZone.lng === "") {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Error!",
+        text: "Please enter delivery location.",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return;
+    }
+    const distance = calculateDistance(
+      myLocation.lat,
+      deliveryZone.lat,
+      myLocation.lng,
+      deliveryZone.lng
+    );
+    if (distance <= deliveryZone.radius) {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Success!",
+        text: "You are in the delivery zone.",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return;
+    } else {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Error!",
+        text: "You aren't in the delivery zone.",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return;
+    }
+  };
+
+  const handleStoreClick = (store) => {
+    setDeliveryZone({ lat: store.lat, lng: store.lng, radius: store.radius });
   };
 
   // Create new component in the same file
@@ -66,7 +150,9 @@ function App() {
         <button onClick={handleGetLocation} className="btn btn-ghost">
           Get location
         </button>
-        <button className="btn btn-outline">Check zone</button>
+        <button onClick={handleCheckZone} className="btn btn-outline">
+          Check delivery zone
+        </button>
       </div>
       <div className="flex justify-center items-center mx-auto">
         {" "}
@@ -86,7 +172,14 @@ function App() {
           {stores &&
             stores.map((store) => {
               return (
-                <Marker position={[store.lat, store.lng]}>
+                <Marker
+                  eventHandlers={{
+                    click: () => {
+                      handleStoreClick(store);
+                    },
+                  }}
+                  position={[store.lat, store.lng]}
+                >
                   <Popup>
                     <b>{store.name}</b>
                     <p>{store.address}</p>
